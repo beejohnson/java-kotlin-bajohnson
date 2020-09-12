f9f96f27d0d94527c1e698d2733443af1a3b707d
Bree Johnson <breeannettejohnson@gmail.com>
2020-09-12 17:52:22 -0600

    added new tests

--- ObjectBeehive/app/src/test/java/com/example/object_beehive/BeehiveTest.java
+++ ObjectBeehive/app/src/test/java/com/example/object_beehive/BeehiveTest.java
@@ -15,13 +15,25 @@ public class BeehiveTest {
 
     @Test
     public void isDry() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertTrue(instance.isDry());
     }
 
     @Test
     public void getHoneyLevel() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertEquals(0.0, instance.getHoneyLevel(), 0.0);
     }
 
     @Test
     public void setHoneyLevel() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertEquals(0.0, instance.getHoneyLevel(), 0.0);
+        double newHoneyLevel = 1;
+        instance.setHoneyLevel(newHoneyLevel);
+        assertEquals(newHoneyLevel, instance.getHoneyLevel(), 0.0);
     }
 }
