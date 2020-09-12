e8f5823801dfaf23c5411a31ad255564526cc780
Bree Johnson <breeannettejohnson@gmail.com>
2020-09-12 17:28:18 -0600

    Added Tests

--- ObjectBeehive/app/src/main/java/com/example/object_beehive/Beehive.java
+++ ObjectBeehive/app/src/main/java/com/example/object_beehive/Beehive.java
@@ -13,5 +13,30 @@ public class Beehive {
 
     private double height;
 
+    // method (getter)
+    public double getHeight() {
+        return height;
+    }
+
+    // computed property
+    public boolean isDry() {
+        return honeyLevel <= 0.0;
+    }
+
+    private double honeyLevel;
+
+    public double getHoneyLevel() {
+        return honeyLevel;
+    }
+
+    public void setHoneyLevel(double honeyLevel) {
+        if (0 <= honeyLevel && honeyLevel <= height) {
+            this.honeyLevel = honeyLevel;
+        } else {
+            String message = "Invalid honey level " + honeyLevel + "." +
+                    " Must be between 0.0 and " + height + " (inclusive).";
+            throw new IllegalArgumentException(message);
+        }
+    }
 
 }
---
+++ ObjectBeehive/app/src/test/java/com/example/object_beehive/BeehiveTest.java
@@ -0,0 +1,24 @@
+package com.example.object_beehive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeehiveTest {
+
+    @Test
+    public void getHeight() {
+    }
+
+    @Test
+    public void isDry() {
+    }
+
+    @Test
+    public void getHoneyLevel() {
+    }
+
+    @Test
+    public void setHoneyLevel() {
+    }
+}
