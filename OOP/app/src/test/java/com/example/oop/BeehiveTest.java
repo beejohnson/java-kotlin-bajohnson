80fbe8d60e1bd5eff570c32e0424389eb26e65e2
Bree Johnson <breeannettejohnson@gmail.com>
2020-09-22 10:42:45 -0600

    Copied Beehive Objects

---
+++ OOP/app/src/test/java/com/example/oop/BeehiveTest.java
@@ -0,0 +1,40 @@
+package com.example.oop;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeehiveTest {
+
+    @Test
+    public void getHeight() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertEquals(hiveHeight, instance.getHeight(), 0.0);
+    }
+
+    @Test
+    public void isDry() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertTrue(instance.isDry());
+    }
+
+    @Test
+    public void getHoneyLevel() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertEquals(0.0, instance.getHoneyLevel(), 0.0);
+    }
+
+    @Test
+    public void setHoneyLevel() {
+        double hiveHeight = 2.5;
+        Beehive instance = new Beehive(hiveHeight);
+        assertEquals(0.0, instance.getHoneyLevel(), 0.0);
+        double newHoneyLevel = 1;
+        instance.setHoneyLevel(newHoneyLevel);
+        assertEquals(newHoneyLevel, instance.getHoneyLevel(), 0.0);
+    }
+}
