63dd1603dc5ddf56f0cb65c9b483b2c3389523a2
Bree Johnson <breeannettejohnson@gmail.com>
2020-09-23 17:30:31 -0600

    changed examples

---
+++ OOP/app/src/main/java/com/example/oop/Well.java
@@ -0,0 +1,16 @@
+package com.example.oop;
+
+public interface Well {
+    double getDepth();
+    void setDepth(double depth);
+
+    double getLevel();
+    void setLevel(double level);
+
+    default boolean isEmpty() {
+        return getLevel() <= 0.0;
+    }
+
+    String getOwner();
+    void setOwner(String owner);
+}
