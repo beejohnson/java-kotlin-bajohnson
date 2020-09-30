d608e053d2e464bad5a64039f8cb37e1861e64da
Bree Johnson <breeannettejohnson@gmail.com>
2020-09-30 16:21:04 -0600

    copied over class

--- ClassHierarchy/app/src/main/java/com/example/classhierarchy/DefaultHive.java
+++ ClassHierarchy/app/src/main/java/com/example/classhierarchy/DefaultHive.java
@@ -1,4 +1,43 @@
 package com.example.classhierarchy;
 
 public class DefaultHive {
-}
+        public DefaultHive(double height) {
+            if (height > 0.0) {
+                this.height = height;
+            } else {
+                String message = "Invalid height " + height + "." +
+                        " Must be positive.";
+                throw new IllegalArgumentException(message);
+            }
+        }
+
+        private double height;
+
+        // method (getter)
+        public double getHeight() {
+            return height;
+        }
+
+        // computed property
+        public boolean isDry() {
+            return honeyLevel <= 0.0;
+        }
+
+        private double honeyLevel;
+
+        public double getHoneyLevel() {
+            return honeyLevel;
+        }
+
+        public void setHoneyLevel(double honeyLevel) {
+            if (0 <= honeyLevel && honeyLevel <= height) {
+                this.honeyLevel = honeyLevel;
+            } else {
+                String message = "Invalid honey level " + honeyLevel + "." +
+                        " Must be between 0.0 and " + height + " (inclusive).";
+                throw new IllegalArgumentException(message);
+            }
+        }
+
+    }
+
