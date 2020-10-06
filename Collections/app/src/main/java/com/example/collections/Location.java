b772aa79149a1de4816b887eacadc7aafbe1b042
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-06 15:43:00 -0600

    more examples

--- Collections/app/src/main/java/com/example/collections/Location.java
+++ Collections/app/src/main/java/com/example/collections/Location.java
@@ -1,25 +1,40 @@
 package com.example.collections;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import static java.lang.Math.*;
+
 public class Location implements Comparable < Location >{ //comparable to itself
     public double getLatitude() {
+
         return latitude;
     }
 
     public double getLongitude() {
+
         return longitude;
     }
 
     public String getName() {
+
         return name;
     }
 
     private double latitude;
     private double longitude;
     private String name;
+
+    public static double normalizeDegreeAngle(double angle) {
+        while (angle > 180.0) { angle -= 360.0; }
+        while (angle > -180.0) {angle += 360.0; }
+        return angle;
+    }
     public Location(String name, double longitude, double latitude) {
         this.name=name;
-        this.longitude=longitude;
-        this.latitude=latitude;
+        this.longitude=normalizeDegreeAngle(longitude);
+        this.latitude=normalizeDegreeAngle(latitude);
     }
 
     @Override
@@ -39,9 +54,83 @@ public class Location implements Comparable < Location >{ //comparable to itself
         }
         return 0; //you need to define the idea of less than
     }
+
+    @Override
+    public boolean equals(Object to) {
+        return compareTo((Location) to) == 0;
+    }
+
+    public double approximateNauticalMilesTo(Location to) {
+        double lambda1 = toRadians(longitude);
+        double lambda2 = toRadians(to.longitude);
+        double deltaLambda = lambda2 - lambda1;
+        double phi1 = toRadians(latitude);
+        double phi2 = toRadians(to.latitude);
+
+        double sinPhi1 = sin(phi1);
+        double sinPhi2 = sin(phi2);
+        double cosPhi1 = cos(phi1);
+        double cosPhi2 = cos(phi2);
+        double sinDeltaLambda = sin(deltaLambda);
+        double cosDeltaLambda = cos(deltaLambda);
+
+        double a = cosPhi2 * sinDeltaLambda;
+        double b = cosPhi1 * sinPhi2 - sinPhi1 * cosPhi2 * cosDeltaLambda;
+        double c = sinPhi1 * sinPhi2 + cosPhi1 * cosPhi2 * cosDeltaLambda;
+        double delta = toDegrees(atan(sqrt(pow(a, 2) + pow(b, 2))) / c) * 60.0;
+
+        return delta;
+    }
+    public static final double METERS_PER_MILE = 1609.334;
+
+    public double approximateMetersTo(Location to) {
+        return approximateNauticalMilesTo(to)*METERS_PER_MILE;
+    }
+
+    public double approximateMilesTo(Location to) {
+        return approximateMetersTo(to);
+    }
+
     @Override
     public int hashCode() {
-        return 7 * name.hashCode() + 13 * Double.hashCode(this.longitude) + 17 * Double.hashCode(this.latitude);
-
+        return 7*name.hashCode()+13*Double.hashCode(this.longitude)+17*Double.hashCode(this.latitude);
     }
+
+    public static void addNearbyLocationsInNauticalMiles(Collection<Location> results, Location center, double nauticalMiles, Iterable<Location> search)  {
+        for (Location at : search) {
+            if (center.approximateNauticalMilesTo(at) <= nauticalMiles) {
+                results.add(at);
+            }
+        }
+    }
+    public static void addNearbyLocationsInMeters(Collection<Location> results, Location center, double meters, Iterable<Location> search)  {
+        double nauticalMiles = meters;
+        addNearbyLocationsInNauticalMiles(results,center,nauticalMiles, search);
+    }
+
+    public static void addNearbyLocationsInMiles(Collection<Location> results, Location center, double miles, Iterable<Location> search)  {
+        double meters = miles*METERS_PER_MILE;
+        addNearbyLocationsInMeters(results,center,meters, search);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
