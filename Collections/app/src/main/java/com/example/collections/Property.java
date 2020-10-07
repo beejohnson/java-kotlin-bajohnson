ec411bd8f36d9d6badc87165496b02ee2d369b57
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-06 19:24:09 -0600

    Property.java

--- Collections/app/src/main/java/com/example/collections/Property.java
+++ Collections/app/src/main/java/com/example/collections/Property.java
@@ -1,8 +1,108 @@
 package com.example.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Property { //Property has a collection of wells
-    Map< Location, OilWell > oilWells = new TreeMap<>();
+private Map<Location, OilWell> oilWells = new TreeMap<Location, OilWell>();
+private Map<Location, WaterWell> waterWells = new TreeMap<Location, WaterWell>();
+
+private List<String> owners = new ArrayList<String>(100);
+
+private List<Byte> privateKeySignature = new ArrayList<>(256);
+void boolExample() {
+    ArrayList < Boolean > yns = new ArrayList<>();
+    yns.add(Boolean.valueOf(true));
+    yns.add(new Boolean(true));
+    yns.add(new Boolean(false));
+    yns.add(new Boolean(true));
+    yns.add(new Boolean(true));
 }
+
+public void addToKeySignature(byte value) {
+    privateKeySignature.add(value);
+    privateKeySignature.add(Byte.valueOf(value));
+// done as        privateKeySignature.add((Object) new Byte(value));
+    byte thirdValue = privateKeySignature.get(2);
+    byte thirdValueLonghandWay = ((Byte) (Object) privateKeySignature.get(2)).byteValue();
+    privateKeySignature.sort((a,b) -> a-b);
+    privateKeySignature.sort((a,b)->a.compareTo(b));
+    List<String> lines = Arrays.asList("spring", "node", "mkyong");
+
+    lines.parallelStream().filter(p->p.endsWith("g")).forEachOrdered(System.out::println);              // convert list to stream
+
+    List<String> result = lines.parallelStream()                // convert list to stream
+            .filter(line -> !"mkyong".equals(line))     // we dont like mkyong
+            .collect(Collectors.toList());              // collect the output and convert streams to a List
+
+    result.forEach(System.out::println);
+}
+
+    public void addOilWellAt(Location at, OilWell oilWell) {
+        oilWells.put(at, oilWell);
+    }
+
+    public void addWaterWellAt(Location at, WaterWell waterWell) {
+        waterWells.put(at, waterWell);
+    }
+
+    public Set<Location> getOilWellLocations() {
+        return oilWells.keySet();
+    }
+
+    public Set<Location> getWaterWellLocations() {
+        return waterWells.keySet();
+    }
+
+    public WaterWell getWaterWellAt(Location at) {
+        return waterWells.get(at);
+    }
+
+    public OilWell getOilWellAt(Location at) {
+        return oilWells.get(at);
+    }
+
+    public Set<Location> waterWellsNearInMeters(Location center, double cutoffInMeters) {
+        TreeSet<Location> nearby = new TreeSet<Location>();
+        Location.addNearbyLocationsInMeters(nearby, center, cutoffInMeters, waterWells.keySet());
+        return nearby;
+    }
+
+    public Set<Location> oilWellsNearInMeters(Location center, double cutoffInMeters) {
+        TreeSet<Location> nearby = new TreeSet<Location>();
+        Location.addNearbyLocationsInMeters(nearby, center, cutoffInMeters, oilWells.keySet());
+        return nearby;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Collections/app/src/test/java/com/example/collections/LocationTest.java
@@ -0,0 +1,60 @@
+package com.example.collections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LocationTest {
+
+    @Test
+    public void getLatitude() {
+    }
+
+    @Test
+    public void getLongitude() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void normalizeDegreeAngle() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void approximateNauticalMilesTo() {
+    }
+
+    @Test
+    public void approximateMetersTo() {
+    }
+
+    @Test
+    public void approximateMilesTo() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void addNearbyLocationsInNauticalMiles() {
+    }
+
+    @Test
+    public void addNearbyLocationsInMeters() {
+    }
+
+    @Test
+    public void addNearbyLocationsInMiles() {
+    }
+}
