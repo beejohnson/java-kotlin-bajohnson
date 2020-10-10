7a6fcee43116ddc61eac11a60f41bb8f5334b778
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-10 13:04:21 -0600

    new code

--- BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Location.java
+++ BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Location.java
@@ -3,19 +3,78 @@ package com.example.beehivecollections;
 import java.util.Collection;
 
 public class Location implements Comparable <Location > {
-    public double getHiveNumber() {
-        return getHiveNumber();
+    public double getHiveX() {
+        return hiveX;
     }
 
-    public double get
+    public double getHiveY() {
+        return hiveY;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+    private double hiveX; //hive location on x-axis
+    private double hiveY; //hive location on y-axis
+
+    public static double normalAxis(double axis) {
+        while (axis > 25.0) { axis -= 100.0;}
+        while (axis <= -25.0) { axis += 100.0;}
+        return axis;
+    }
+
+    public Location(String name, double hiveX, double hiveY) {
+        this.name = name;
+        this.hiveX=normalAxis(hiveX);
+        this.hiveY=normalAxis(hiveY);
+    }
+
+    @Override
+    public int compareTo(Location other) {
+        int cmp;
+        cmp = this.name.compareTo(other.name);
+        if(cmp !=0) {
+            return cmp;
+        }
+        cmp = (this.hiveX < other.hiveX) ? -1 : (this.hiveX > other.hiveX) ? 1 : 0;
+        if (cmp != 0) {
+            return cmp;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object to) {
+        return compareTo((Location) to) == 0;
+    }
+
+    public double metersTo(Location to) {
+        double x = 1;
+        double y = 1;
+        double area = x * y;
+
+        return area;
+    }
+
+    public double centimetersTO(Location to) {
+        return metersTo(to)*10;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode()+Double.hashCode(this.hiveX)+Double.hashCode(this.hiveY);
+    }
+
+    public static void addLocationInMeters(Collection<Location> results, Location center, double meters, Iterable<Location> search) {
+        for (Location at : search) {
+            if (center.centimetersTO(at) <= meters) {
+                results.add(at);
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
 
 
 
--- BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Property.java
+++ BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Property.java
@@ -1,4 +1,42 @@
 package com.example.beehivecollections;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class Property {
+    private Map<Location, BeeHive> beehives = new TreeMap<Location, BeeHive>();
+    private Map<Location, Apiary> apiaries = new TreeMap<Location, Apiary>();
+
+    private List<String> owners = new ArrayList<String> (100);
+
+    private List<Byte> privateKeySignature = new ArrayList<Byte> (256);
+    void boolEx() {
+        ArrayList < Boolean > yns = new ArrayList<>();
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
