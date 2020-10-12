03b7a8d4cab8fe3c70b1e769539acc682a4e8d51
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-11 18:31:03 -0600

    finished Property.java

--- BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Property.java
+++ BeeHiveCollections/app/src/main/java/com/example/beehivecollections/Property.java
@@ -19,10 +19,76 @@ public class Property {
     private List<Byte> privateKeySignature = new ArrayList<Byte> (256);
     void boolEx() {
         ArrayList < Boolean > yns = new ArrayList<>();
+        yns.add(Boolean.valueOf(true));
+        yns.add(new Boolean(true));
+        yns.add(new Boolean(false));
+        yns.add(new Boolean(true));
+        yns.add(new Boolean(true));
+    }
 
+    public void addToKeySignature(byte value) {
+        privateKeySignature.add(value);
+        privateKeySignature.add(Byte.valueOf(value));
+
+        byte thirdValue = privateKeySignature.get(2);
+        byte thirdValueLonghandWay = ((Byte) (Object) privateKeySignature.get(2)).byteValue();
+        privateKeySignature.sort((a,b) -> a-b);
+        privateKeySignature.sort((a,b)->a.compareTo(b));
+        List<String> lines = Arrays.asList("spring", "node", "mkyong");
+
+        lines.parallelStream().filter(p->p.endsWith("g")).forEachOrdered(System.out::println);
+
+        List<String> result = lines.parallelStream()
+                .filter(line -> !"mkyong".equals(line))
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
+    }
+
+    public void addBeeHiveAt(Location at, BeeHive beeHive) {
+        beehives.put(at, beeHive);
+    }
+
+    public void addApiaryAt(Location at, Apiary apiary) {
+        apiaries.put(at, apiary);
+    }
+
+    public Set<Location> getBeeHiveLocations() {
+        return beehives.keySet();
+    }
+
+    public Set<Location> getApiaryLocations() {
+        return apiaries.keySet();
+    }
+
+    public BeeHive getBeeHiveAt(Location at) {
+        return beehives.get(at);
+    }
+
+    public Apiary getApiaryAt(Location at) {
+        return apiaries.get(at);
+    }
+
+    public Set<Location> ApiaryInMeters(Location center, double cutOffInMeters){
+        TreeSet<Location> nearby = new TreeSet<Location>();
+        Location.addLocationInMeters(nearby, center, cutOffInMeters, apiaries.keySet());
+        return nearby;
+    }
+
+    public Set<Location> BeeHiveInMeters(Location center, double cutOffInMeters) {
+        TreeSet<Location> nearby = new TreeSet<Location>();
+        Location.addLocationInMeters(nearby, center, cutOffInMeters, beehives.keySet());
+        return nearby;
     }
 
 }
+
+
+
+
+
+
+
+
 
 
 
