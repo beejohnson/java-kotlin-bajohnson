0af6c7a4516d5f4e5a5be9f1605ae35f683e7b07
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-24 14:25:59 -0600

    Sprite Display Android app

---
+++ SpriteDisplay/app/src/main/java/com/example/spritedisplay/GameView.java
@@ -0,0 +1,32 @@
+package com.example.spritedisplay;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+}
