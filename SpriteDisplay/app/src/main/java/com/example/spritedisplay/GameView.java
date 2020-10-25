d955449d7ea98b5654ca0862fe8e28221333862d
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-25 13:32:17 -0600

    Adding sprite movement

--- SpriteDisplay/app/src/main/java/com/example/spritedisplay/GameView.java
+++ SpriteDisplay/app/src/main/java/com/example/spritedisplay/GameView.java
@@ -1,6 +1,8 @@
 package com.example.spritedisplay;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -8,11 +10,15 @@ import androidx.annotation.NonNull;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    private Sprite sprite;
 
     public GameView(Context context) {
         super(context);
 
         getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+        setFocusable(true);
     }
 
     @Override
@@ -22,16 +28,36 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        sprite = new Sprite(BitmapFactory.decodeResource(getResources(), R.drawable.bee));
 
+        thread.setRunning(true);
+        thread.start();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
     }
 
     public void update() {
+        sprite.update();
+    }
 
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if(canvas!=null) {
+            sprite.draw(canvas);
+        }
     }
 
 }
--- SpriteDisplay/app/src/main/java/com/example/spritedisplay/Sprite.java
+++ SpriteDisplay/app/src/main/java/com/example/spritedisplay/Sprite.java
@@ -1,4 +1,24 @@
 package com.example.spritedisplay;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
 public class Sprite {
+
+    private final Bitmap image;
+    private int x, y;
+
+    public Sprite(Bitmap bmp) {
+        image = bmp;
+        x = 100;
+        y = 100;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+    }
+
+    public void update() {
+        y++;
+    }
 }
