c40ba8db70986c1754d13e2dddda485f3f5d7ed1
Bree Johnson <breeannettejohnson@gmail.com>
2020-10-31 11:08:13 -0600

    initial commit

---
+++ addingWords/app/src/test/java/com/example/addingwords/AddingWordsTest.java
@@ -0,0 +1,32 @@
+package com.example.addingwords;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddingWordsTest {
+
+    @Test
+    public void doClear() {
+    }
+
+    @Test
+    public void doDef() {
+    }
+
+    @Test
+    public void op() {
+    }
+
+    @Test
+    public void doCalc() {
+    }
+
+    @Test
+    public void doCommand() {
+    }
+
+    @Test
+    public void readCommand() {
+    }
+}
